fe20787b336c549ba0da96b7efeba88aa5196a36
Kalidosssubramaniyan20 <kalidosssubaramaniyan20@gmail.com>
2026-04-17 22:08:36 +0530

    "Final commit"

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api"
+  baseURL: "https://firstproject-1-tm8k.onrender.com/api/products"
 });
 
 export default API;
